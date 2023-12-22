d181ab227ff7d449cc84e88d308903a0392ace0f
jcarlson <jake.carlson90@gmail.com>
2023-12-21 17:44:07 -0800

    creating the slot machine

--- gamble.js
+++ gamble.js
@@ -9,8 +9,26 @@
 
 const prompt = require('prompt-sync')();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
+}
+
+const SYMBOL_VALUES = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
+}
+
 const deposit = () => {
     while (true) {
+
         const depositAmount = prompt('Enter a Deposit Amount: ');
         const numberDepositAmount = parseFloat(depositAmount);
 
@@ -22,5 +40,45 @@ const deposit = () => {
     }
 };
 
-const depositAmount = deposit();
-console.log(depositAmount)
+const getNumberOfLines = () => {
+    while (true) {
+
+        const lines = prompt('Enter Number of Lines to Bet On (1-3): ');
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log('Invalid Number Of Lines, Try Again.')
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
+
+const getBet = (balance, lines) => {
+    while (true) {
+
+        const bet = prompt('Enter Bet Per Line: ');
+        const numberBet = parseFloat(bet);
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log('Invalid Bet, Try Again.')
+        } else {
+            return numberBet;
+        }
+    }
+};
+
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+};
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+
